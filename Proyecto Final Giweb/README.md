aa97b774a4a69c86f36e58bf728c939a783afb68
Brayan-22 <72152632+Brayan-22@users.noreply.github.com>
2022-04-05 03:33:31 -0500

    Final

---
+++ Proyecto Final Giweb/README.md
@@ -0,0 +1,18 @@
+# Carrito de compras - Proyecto Final
+
+
+
+## Previsualización:
+
+
+[![carrito.png](https://i.postimg.cc/BnRzdK7V/carrito.png)](https://postimg.cc/pmYkjpHf)
+
+
+## Desarrollador👨‍💻
+
+* Brayan Alejandro Riveros Rodríguez 
+
+
+Grupo de trabajo GIWEB de la Universidad Distrital
+
+_Estudiante de Ingenieria de Sistemas de la Universidad Distrital_
--- Proyecto Final Giweb/app.js
+++ Proyecto Final Giweb/app.js
@@ -6,9 +6,12 @@ const fragment = document.createDocumentFragment()
 
 const buttonsGroup=document.getElementById('grupoBotones')
 
+
+
 document.addEventListener('DOMContentLoaded',()=>{
     fetchData('apiRestaurant.json')
 })
+
 
 
 const fetchData = async (caso)=>{
@@ -61,9 +64,32 @@ const pintarCards= data=>{
         templateCard.querySelector('h5').textContent = producto.title
         templateCard.querySelector('p').textContent=producto.precio
         templateCard.querySelector('img').setAttribute("src",producto.img)
-        templateCard.querySelector('button').setAttribute("data-button",producto.precio)
+        templateCard.querySelector('.btn-comprar').setAttribute("data-button",'comprar')
+        templateCard.querySelector('.btn-eliminar').setAttribute("data-button",'eliminar')
+        templateCard.querySelector('.valor').setAttribute("id","valor"+producto.title)
+        templateCard.querySelector('.valor').textContent='0'
         const clone = templateCard.cloneNode(true)
         fragment.appendChild(clone)
     });
     items.appendChild(fragment)
 }
+
+
+//const value=document.querySelector('#valor')
+//var cantidad=parseInt(value.textContent)
+//const buttonsGroup2=document.getElementById('grupoBotonesCompras')
+//buttonsGroup2.addEventListener('click',(e)=>{
+//    let caso=e.target.dataset.button;
+//    try {
+//        if(caso=='comprar' && cantidad>=0){
+//            cantidad+=1
+//        }else if(caso=='eliminar' && cantidad>0){
+//            cantidad-=1
+//        }else{
+//            cantidad=0
+//        }
+//        value.textContent=cantidad.toString()
+//        console.log(value.textContent);
+//    } catch (error) {
+//    }
+//})
